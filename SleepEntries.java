501a0f9563a0b18271e787b6814ccafddfeb5079
1948771 <1948771@01-18108.pusd.dom>
2025-05-23 12:04:09 -0700

    added to sleep entries

--- SleepEntries.java
+++ SleepEntries.java
@@ -1,3 +1,25 @@
+
+
 public class SleepEntries {
+    private String date; // can make calendar UI to select date
+    private int sleepTimeHrs;
+    private int sleepTimeMins;
+    private int wakeTimeHrs;
+    private int wakeTimeMins;
+    private boolean isAM;
     
+    public SleepEntries(String d, int sth, int stm, int wth, int wtm, boolean am) {
+    	date = d;
+    	sleepTimeHrs = sth;
+    	sleepTimeMins = stm;
+    	wakeTimeHrs = wth;
+    	wakeTimeMins = wtm;
+    	isAM = am;
+    }
+    
+    public int getSleepDuration() {
+    	
+    	
+    	return 
+    }
 }
